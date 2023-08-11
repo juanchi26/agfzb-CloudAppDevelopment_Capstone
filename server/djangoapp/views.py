39c808b5a2c570507e645397eb9b103f2bab7802
Juan Gabriel Díaz <106716500+juanchi26@users.noreply.github.com>
2023-08-11 20:16:58 -0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
+from .restapis import get_dealer_reviews_from_cf, post_request, get_dealers_from_cf, get_dealer_by_id_from_cf
+from .models import CarDealer, CarMake, CarModel, DealerReview
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -85,11 +86,67 @@ def get_dealerships(request):
         return render(request, 'djangoapp/index.html', context)
 
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+# Update the `get_dealerships` view to render the index page with a list of dealerships
+def get_dealerships(request):
+    context = {}
+    if request.method == "GET":
+        url = "https://us-east.functions.appdomain.cloud/api/v1/web/coursera-1c4_coursera-us-east/dealership-package/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        context['dealership_list'] = dealerships
+        # Return a list of dealer short name
+        return render(request, 'djangoapp/index.html', context)
 
+
+# Create a `get_dealer_details` view to render the reviews of a dealer
+def get_dealer_details(request, dealer_id):
+    context = {}
+    dealerships = get_dealer_by_id_from_cf(
+        url="https://us-east.functions.appdomain.cloud/api/v1/web/coursera-1c4_coursera-us-east/dealership-package/dealership", 
+        dealerId=dealer_id
+    )
+    context["dealer_name"] = dealerships[0].name
+    context["dealer_id"] = dealer_id
+
+    url = "https://us-east.functions.appdomain.cloud/api/v1/web/coursera-1c4_coursera-us-east/dealership-package/review"
+    reviews = get_dealer_reviews_from_cf(url, dealer_id)
+    context['review_list'] = reviews
+    return render(request, 'djangoapp/dealer_details.html', context) # also sentiment?
+
+    
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):    
+    context = {}    
+    url = "https://eu-gb.functions.appdomain.cloud/api/v1/web/coursera-1c4_capstone-eu/dealership-package/post-review.json"
+    
+    if not request.user.is_authenticated:
+        redirect('djangoapp/registration.html')
+    else:
+        if request.method == "POST":
+            review = dict()
+            review["time"] = datetime.utcnow().isoformat()
+            review["dealership"] = dealer_id
+            review["name"] = username
+            review["review"] = request.POST["content"]
+            review["purchase"] = False
+            if "purchasecheck" in request.POST:
+                if request.POST["purchasecheck"] == 'on':
+                    review["purchase"] = True
+            review["purchase_date"] = request.POST["purchasedate"]
+            review["car_make"] = car.make.name
+            review["car_model"] = car.name
+            review["car_year"] = int(car.year.strftime("%Y"))
+            
+            json_payload = dict()
+            json_payload["review"] = review
+            response = post_request(url=post_url, payload=json_payload)
+            print(response)
+            return redirect("djangoapp:dealer_details", dealer_id)
+        # this redirect will be routed to get_dealer_detailers() above
+        elif request.method == "GET":
+            cars = CarModel.objects.filter(dealer_id=dealer_id)
+            print(cars)
+            context["cars"] = cars
+            return render(request, 'djangoapp/add_review.html', context)
 
